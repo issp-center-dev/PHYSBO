84a77f2441a96b2d1ce4a46c62811e8b0db996b8
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2021-01-15 13:00:45 +0900

    fix bug for compiling tex

--- docs/sphinx/manual/ja/source/conf.py
+++ docs/sphinx/manual/ja/source/conf.py
@@ -25,9 +25,9 @@ copyright = "2020, PHYSBO developers"
 author = "PHYSBO developers"
 
 # The short X.Y version.
-version = '0.1'
+version = '1.0-beta'
 # The full version, including alpha/beta/rc tags.
-release = '0.1.0'
+release = '1.0.0-beta'
 
 # -- General configuration ---------------------------------------------------
 
