f476e51a4d9813601d34e2fe1013e1284cbec281
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2022-06-20 16:24:16 +0900

    change version number (1.2-dev)

--- docs/sphinx/manual/en/source/conf.py
+++ docs/sphinx/manual/en/source/conf.py
@@ -32,9 +32,9 @@ copyright = "2020-, PHYSBO developers"
 author = "PHYSBO developers"
 
 # The short X.Y version.
-version = "1.1"
+version = "1.2"
 # The full version, including alpha/beta/rc tags.
-release = "1.1.0"
+release = "1.2-dev"
 
 # for jupyter-notebook
 
