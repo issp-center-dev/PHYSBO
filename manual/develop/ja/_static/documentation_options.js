7b405a421dbe030acc90fbcc8798ec1bd4244d8d
PHYSBO Developers <physbo-dev@issp.u-tokyo.ac.jp>
2024-03-11 02:02:02 +0000

    Deploy docs to develop by GitHub Actions triggered by 307119c0aebeeb4eae3c1dda4401ee7a5791bde8

--- manual/develop/ja/_static/documentation_options.js
+++ manual/develop/ja/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2-dev',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.1.1',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
