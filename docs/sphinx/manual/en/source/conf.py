bf02068117857584175c95c591cd6ee5cbeb4d44
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2025-01-29 16:30:43 +0900

    v2.1.0

--- docs/sphinx/manual/en/source/conf.py
+++ docs/sphinx/manual/en/source/conf.py
@@ -34,7 +34,7 @@ author = "PHYSBO developers"
 # The short X.Y version.
 version = "2.1"
 # The full version, including alpha/beta/rc tags.
-release = "2.1-dev"
+release = "2.1.0"
 
 # for jupyter-notebook
 
