bb295f8969f02a4dfadf689b5fc1a6e127a563a9
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2025-10-24 17:48:11 +0900

    change version number as 3.2-dev

--- docs/sphinx/manual/en/source/conf.py
+++ docs/sphinx/manual/en/source/conf.py
@@ -33,9 +33,9 @@ copyright = "2020-, PHYSBO developers"
 author = "PHYSBO developers"
 
 # The short X.Y version.
-version = "3.1"
+version = "3.2"
 # The full version, including alpha/beta/rc tags.
-release = "3.1.0"
+release = "3.2-dev"
 
 # for jupyter-notebook
 
