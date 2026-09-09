f44c9eab6ef63a3dd1689506b5cc445350a1f8ec
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2026-09-09 11:32:58 +0900

    v3.2.1

--- docs/sphinx/manual/ja/source/conf.py
+++ docs/sphinx/manual/ja/source/conf.py
@@ -31,7 +31,7 @@ author = "PHYSBO developers"
 # The short X.Y version.
 version = "3.2"
 # The full version, including alpha/beta/rc tags.
-release = "3.2.0"
+release = "3.2.1"
 
 # -- General configuration ---------------------------------------------------
 
